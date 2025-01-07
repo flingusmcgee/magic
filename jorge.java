e84e85bbbbbb1ff89a89bc3f8d080c82fab7b576
flingusmcgee <luis-tay@outlook.com>
2025-01-06 19:34:00 -0800

    Update jorge.java
    Added an exclaimer

--- jorge.java
+++ jorge.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class jorge {
-  public static void main(String[] args) {
-    System.out.println("Fix me!");
+    public static void main(String[] args) {
+      System.out.println("Fix me!");
+      try (Scanner scan = new Scanner(System.in)) {
+        String george = scan.nextLine();
+        System.out.println(exclaim(george));
+      }
+    }
+    public static String exclaim(String input) {
+      return input.concat("!");
   }
 }
